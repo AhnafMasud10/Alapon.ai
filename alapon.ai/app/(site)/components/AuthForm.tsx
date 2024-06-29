48a9958b08fa7a72efc35b15635b17f72915c9c2
AhnafMasud10 <ahnafmasud11@gmail.com>
2024-06-29 16:57:03 +0600

    auth form changed

--- alapon.ai/app/(site)/components/AuthForm.tsx
+++ alapon.ai/app/(site)/components/AuthForm.tsx
@@ -1,10 +1,53 @@
 'use client';
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+import { useForm, FieldValues, SubmitHandler } from 'react-hook-form';
 
 const AuthForm = () => {
-  return (
+  type Variant = 'LOGIN' | 'REGISTER';
+  
+  const[ variant, setVariant] = useState<Variant>('LOGIN');
+  const[isLoading,setIsLoading] = useState(false);
+  const toggleVariant = useCallback(() =>{ //useCallback optimises. login to register happens once but it doesnt need to go from register to login.
+    if(variant === 'LOGIN'){
+      setVariant('REGISTER')
+    }else {
+      setVariant('LOGIN')
+    }
+  },[variant])
+
+  const {
+    register,
+    handleSubmit,
+    formState: {
+      errors
+    }
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: ''
+    }
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data)=> {
+    setIsLoading(true);
+    if(variant=== 'REGISTER'){
+      //axios register
+    }
+    if (variant==='LOGIN'){
+      //next auth sign in
+    }
+  }
+
+  const socialAction = (action:string) => {
+    setIsLoading(true);
+    //nextauth social sign in
+  }
+    return (
     <div>
-        AuthForm
+        className="
+        mt-8  
+        "
     </div>
   )
 }
